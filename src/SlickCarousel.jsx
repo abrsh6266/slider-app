6889892310218c310e57332f6d2d6c2228a2f451
Abrham <abrsh6266@gmail.com>
2024-02-17 23:44:31 -0900

    Finalizing the project

--- src/SlickCarousel.jsx
+++ src/SlickCarousel.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { list } from "./data";
+import { FaQuoteRight } from "react-icons/fa";
+import { useState } from "react";
 const SlickCarousel = () => {
+  const [people, setPeople] = useState(list);
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+  };
   return (
-    <div>SlickCarousel</div>
-  )
-}
-
-export default SlickCarousel
+    <section className="slick-container">
+      <Slider {...settings}>
+        {people.map((p) => {
+          const { name, image, id, quote, title } = p;
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h5 className="name">{name}</h5>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+      </Slider>
+    </section>
+  );
+};
+export default SlickCarousel;
